0dcccff578b1f7c909f668e1d20815e441c97e90
Lumikeiju <167577930+Lumikeiju@users.noreply.github.com>
2025-10-26 16:41:43 -0700

    Add config files

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+/** @format */
+
+module.exports = {
+    semi: true,
+    singleQuote: false,
+    trailingComma: "es5",
+    bracketSpacing: true,
+    arrowParens: "always",
+    proseWrap: "preserve",
+    insertPragma: true,
+    requirePragma: false,
+};
+
